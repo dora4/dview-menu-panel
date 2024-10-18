35a2d8de4bca4a77b340ed82f1f3986a7a389c3c
dora <dora924666990@gmail.com>
2024-10-19 03:07:36 +0800

    支持修改输入菜单光标颜色

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -35,7 +35,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = rootProject.project.name
-                version = "1.26"
+                version = "1.27"
             }
         }
     }
