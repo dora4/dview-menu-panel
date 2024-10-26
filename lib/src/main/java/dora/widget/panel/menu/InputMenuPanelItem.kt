102172f45c9b53a488a14211213a3399d7df7281
dora <dora924666990@gmail.com>
2024-10-26 17:15:02 +0800

    新增ContentWriter

--- lib/src/main/java/dora/widget/panel/menu/InputMenuPanelItem.kt
+++ lib/src/main/java/dora/widget/panel/menu/InputMenuPanelItem.kt
@@ -6,10 +6,8 @@ import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.LinearLayout
 import dora.widget.panel.R
 import dora.widget.panel.MenuPanelItem
@@ -78,7 +76,7 @@ class InputMenuPanelItem
                 override fun afterTextChanged(s: Editable) {}
             })
         }
-        val arrowView = menuView.findViewById<ImageView>(ID_LINEAR_LAYOUT_ARROW)
+        val arrowView = menuView.findViewById<LinearLayout>(ID_LINEAR_LAYOUT_ARROW)
         if (showArrowIcon) {
             arrowView.visibility = View.VISIBLE
             arrowView.setOnClickListener {
